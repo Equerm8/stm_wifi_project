dfdd9cca2aff0300675a392550369916b6b22400
Marcel <118558793+Equerm8@users.noreply.github.com>
2026-05-13 21:59:44 +0200

    implemented update funcs in auxiliary funcs

--- display_unit/Core/Inc/auxiliary_functions.h
+++ display_unit/Core/Inc/auxiliary_functions.h
@@ -4,6 +4,7 @@
 #include "main.h"
 
 void analyze_mess(void);
+void display_text_at_row(const char *text, const DisplayRow *row);
 void show_wifi(bool state);
 void update_disp_val(uint32_t value, const DisplayRow *row);
 
--- display_unit/Debug/Core/Src/operational_variables.d
+++
@@ -1,3 +0,0 @@
-Core/Src/operational_variables.o: ../Core/Src/operational_variables.c \
- ../Core/Inc/operational_variables.h
-../Core/Inc/operational_variables.h:
